68530063ba2c42b8342bc4258f64cdffffce95f8
Sergei W. Adonev <admin@cnlist.us>
2018-10-24 18:58:56 +0500

    CYP-4 Инициализировать проект - RESTY поддержка JSON-API

--- src/main/java/us/cnlist/cop/Application.java
+++ src/main/java/us/cnlist/cop/Application.java
@@ -1,12 +1,13 @@
 package us.cnlist.cop;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args){
-
+        SpringApplication.run(Application.class);
     }
 
 
