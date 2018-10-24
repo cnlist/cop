2f60bc319fb9cdb4bd9b4b6a4ab620f3408cdddb
Sergei W. Adonev <admin@cnlist.us>
2018-10-24 18:03:32 +0500

    Initial commit

---
+++ src/main/java/us/cnlist/cop/Application.java
@@ -0,0 +1,13 @@
+package us.cnlist.cop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+
+    }
+
+
+}
---
+++ src/main/java/us/cnlist/cop/client/CopClient.java
@@ -0,0 +1,9 @@
+package us.cnlist.cop.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+
+public class CopClient implements EntryPoint {
+    public void onModuleLoad() {
+    }
+}
