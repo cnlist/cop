9fb9abaf5095eb26d75e59bc2ee7f98dfd314d95
markkosivhkin <mekosichkin.ru>
2018-10-27 16:45:00 +0300

    Сделал простой контроллер вывода информации о текущем пользователе. Теперь защищена именно эта страница а не  products

--- src/main/java/us/cnlist/cop/config/SecurityConfig.java
+++ src/main/java/us/cnlist/cop/config/SecurityConfig.java
@@ -1,38 +1,42 @@
 package us.cnlist.cop.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class DevSecurity extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource) throws Exception {
+    public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource, PasswordEncoder encoder) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource)
                 .usersByUsernameQuery("select username, password, enabled"
                         + " from users where username=?")
                 .authoritiesByUsernameQuery("select username, authority "
                         + "from authorities where username=?")
-                .passwordEncoder(new BCryptPasswordEncoder());
+                .passwordEncoder(encoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .mvcMatchers("/products.html").access("hasRole('ROLE_ADMIN')")
+                .mvcMatchers("/user_info").access("hasRole('ROLE_USER')")
                 .and().formLogin().defaultSuccessUrl("/", false);
 
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
---
+++ src/main/java/us/cnlist/cop/controller/SecurityExample.java
@@ -0,0 +1,15 @@
+package us.cnlist.cop.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SecurityExample {
+    @RequestMapping("/user_info")
+    @ResponseBody
+    public String test() {
+        return SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+    }
+}
