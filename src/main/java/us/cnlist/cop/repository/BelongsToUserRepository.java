abf7c4289e1132b09611a62806959c1bf262dfb6
markkosivhkin <mekosichkin.ru>
2018-11-01 15:31:57 +0300

    Добавил контроллер на deviceGroup

--- src/main/java/us/cnlist/cop/repository/BelongsToUserRepository.java
+++ src/main/java/us/cnlist/cop/repository/BelongsToUserRepository.java
@@ -1,7 +1,5 @@
 package us.cnlist.cop.repository;
 
-import org.springframework.data.repository.CrudRepository;
-
 public interface BelongsToUserRepository<T> {
 
     Iterable<T> findAllByUsername(String username);
